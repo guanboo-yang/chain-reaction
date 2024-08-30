d7368f2006b4bbd0a63200966b638f2910e6b400
guanboo-yang <b09902064@ntu.edu.tw>
2024-08-30 21:16:27 +0800

    feat: init game

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,206 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PropsWithChildren, useEffect, useRef, useState } from "react";
+import Piece from "./piece";
+import { Color } from "./color";
+import { cn } from "./cn";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Cell = {
+  value: number;
+  color: Color;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function initBoard(width: number, height: number) {
+  return Array.from({ length: width * height }, () => ({
+    value: 0,
+    color: Color.None,
+  }));
 }
 
-export default App
+function critical(width: number, height: number, index: number) {
+  return [
+    index > width - 1,
+    index % width !== 0,
+    index < width * (height - 1),
+    index % width !== width - 1,
+  ].filter(Boolean).length;
+}
+
+export default function App() {
+  const intervalRef = useRef<number | null>(null);
+  const [iter, setIter] = useState(-1);
+  const [pending, setPending] = useState(false);
+  const [board, setBoard] = useState<Cell[]>(initBoard(5, 5));
+  const [isOver, setIsOver] = useState(false);
+
+  const red = board.filter((cell) => cell.color === Color.Red).length;
+  const blue = board.filter((cell) => cell.color === Color.Blue).length;
+
+  const current = iter === -1 ? Color.None : iter % 2;
+
+  function handleClick({ value, color }: Cell, index: number) {
+    if (iter === -1) return false;
+    // can place on empty cell or same color
+    if (color !== Color.None && color !== current) return false;
+    setPending(true);
+    setBoard((prev) => {
+      const next = [...prev];
+      next[index].value = value + 1;
+      next[index].color = current;
+      return next;
+    });
+    return true;
+  }
+
+  function handleRestart() {
+    setIter(-1);
+    setBoard(initBoard(5, 5));
+    setIsOver(false);
+    setPending(false);
+  }
+
+  useEffect(() => {
+    if (iter === -1) return;
+    if (iter >= 2 && (blue === 0 || red === 0)) setIsOver(true);
+    if (board.every((cell, index) => cell.value < critical(5, 5, index))) {
+      setPending(false);
+      if (!isOver) setIter((prev) => prev + 1);
+      return;
+    }
+    intervalRef.current = setTimeout(() => {
+      setBoard((prev) => {
+        const indices = prev
+          .map((cell, i) => (cell.value < critical(5, 5, i) ? -1 : i))
+          .filter((index) => index !== -1);
+        const add = Array.from({ length: 25 }, () => 0);
+        for (const index of indices) {
+          if (index > 4) add[index - 5]++;
+          if (index % 5 !== 0) add[index - 1]++;
+          if (index < 20) add[index + 5]++;
+          if (index % 5 !== 4) add[index + 1]++;
+        }
+        return prev.map((cell, index) => {
+          if (indices.includes(index)) {
+            if (cell.value === critical(5, 5, index)) {
+              return { value: 0, color: Color.None };
+            }
+            return {
+              value: cell.value - critical(5, 5, index),
+              color: cell.color,
+            };
+          }
+          if (add[index]) {
+            return {
+              value: cell.value + add[index],
+              color: current,
+            };
+          }
+          return cell;
+        });
+      });
+    }, 500);
+  }, [JSON.stringify(board)]);
+
+  useEffect(() => {
+    if (isOver) return;
+    setTimeout(() => setIter(0), 500);
+  }, [isOver]);
+
+  // useEffect(() => {
+  //   if (current !== Color.Red || pending) return;
+  //   fetch("http://localhost:8080/api", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(board),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const index = data.index;
+  //       handleClick(board[index], index);
+  //     });
+  // }, [current, pending]);
+
+  return (
+    <div
+      className={cn(
+        "relative text-4xl w-full flex flex-col items-center duration-300 transition-colors justify-center h-screen",
+        current === Color.None && "bg-neutral-300",
+        current === Color.Blue && "bg-sky-300",
+        current === Color.Red && "bg-red-300"
+      )}
+    >
+      <div className="relative z-10 flex items-center justify-center gap-2 my-4 px-4 py-2 rounded-xl bg-white">
+        <span className="text-2xl text-sky-500">{blue}</span>
+        <span className="text-2xl text-black">·</span>
+        <span className="text-2xl text-red-500">{red}</span>
+      </div>
+      <div
+        className={cn(
+          "grid grid-cols-5 grid-rows-5 gap-2",
+          (pending || iter === -1) && "pointer-events-none"
+        )}
+      >
+        {board.map((piece, index) => (
+          <Cell
+            key={index}
+            color={piece.color}
+            isOver={isOver}
+            current={current}
+            onClick={() => handleClick(piece, index)}
+          >
+            <Piece value={piece.value} color={piece.color} />
+            {/* <Piece value={6} color={Color.Red} /> */}
+          </Cell>
+        ))}
+      </div>
+      <div
+        className={cn(
+          "absolute inset-0 transition-colors duration-500 flex flex-col gap-4 items-center justify-center",
+          isOver ? "opacity-100 bg-black/80" : "opacity-0 pointer-events-none"
+        )}
+      >
+        {current === Color.Blue && <p className="text-sky-500">Blue Wins!</p>}
+        {current === Color.Red && <p className="text-red-500">Red Wins!</p>}
+        <button
+          onClick={handleRestart}
+          className="bg-white py-2 px-4 rounded-xl hover:bg-neutral-200"
+        >
+          Restart
+        </button>
+      </div>
+      {/* add up all value */}
+      <span className="text-2xl py-6">
+        {board.reduce((acc, cur) => acc + cur.value, 0)}
+        {/* {iter === -1 ? 0 : iter} */}
+      </span>
+    </div>
+  );
+}
+
+interface CellProps {
+  color: Color;
+  isOver: boolean;
+  current: Color;
+  onClick: () => void;
+}
+
+function Cell(props: PropsWithChildren<CellProps>) {
+  const isValid =
+    !props.isOver &&
+    (props.color === Color.None || props.color === props.current);
+  return (
+    <button
+      onClick={props.onClick}
+      className={cn(
+        "bg-neutral-50/80 transition-[filter,background] rounded-xl size-24 flex shadow-md items-center justify-center",
+        props.current === Color.Red && "bg-red-200",
+        props.current === Color.Blue && "bg-sky-200",
+        props.color === Color.Red && "bg-red-200",
+        props.color === Color.Blue && "bg-sky-200",
+        isValid ? "hover:brightness-105" : "brightness-90"
+      )}
+    >
+      {props.children}
+    </button>
+  );
+}
---
+++ src/piece.tsx
@@ -0,0 +1,59 @@
+import { cn } from "./cn";
+import { Color } from "./color";
+
+interface PieceProps {
+  value: number;
+  color: Color;
+}
+
+const rotations: {
+  [key: number]: number[];
+} = {
+  2: [90, 270],
+  3: [180, 420, 300],
+  4: [270, 540, 450, 360],
+  5: [324, 612, 540, 468, 396],
+  6: [360, 660, 600, 540, 480, 420],
+};
+
+function getRotate(value: number, index: number) {
+  const rotation = rotations[value];
+  if (!rotation) return 0;
+  if (index >= rotation.length) return rotation[0];
+  return rotation[index];
+}
+
+export default function Piece(props: PieceProps) {
+  return (
+    <div
+      className={cn(
+        "relative size-4/5 rounded-full transition-[all] scale-100 duration-500",
+        props.color === Color.None && "opacity-0 scale-75",
+        props.color === Color.Red && "bg-red-500",
+        props.color === Color.Blue && "bg-sky-500"
+      )}
+    >
+      {/* debug */}
+      {/* <div className="absolute left-0 -top-2 text-2xl">{props.value}</div> */}
+      {Array.from({ length: 6 }).map((_, index) => (
+        <div
+          key={index}
+          className="inset-0 absolute w-full h-full transition-[rotate] duration-500"
+          style={{ rotate: `${getRotate(props.value, index)}deg` }}
+        >
+          <div
+            className={cn(
+              "absolute bg-white size-5 transition-[top] duration-500 rounded-full inset-1/2 -translate-x-1/2 -translate-y-1/2",
+              props.value <= 1 && "top-1/2",
+              props.value === 2 && "top-[25%]",
+              props.value === 3 && "top-[76%]",
+              props.value === 4 && "top-[23%]",
+              props.value === 5 && "top-[78%]",
+              props.value === 6 && "top-[21%]"
+            )}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
