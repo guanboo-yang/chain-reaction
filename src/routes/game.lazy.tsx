3cb82807796d1e432eb5f1e8918e6c1444bf2971
guanboo-yang <b09902064@ntu.edu.tw>
2024-08-30 22:19:44 +0800

    feat: extract game status

--- src/routes/game.lazy.tsx
+++ src/routes/game.lazy.tsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { cn } from "../lib/cn";
 import { Color } from "../lib/color";
 import Piece from "../components/piece";
@@ -13,6 +13,13 @@ type Cell = {
   value: number;
   color: Color;
 };
+
+enum Status {
+  Init = "init",
+  Idle = "idle",
+  Pending = "pending",
+  Over = "over",
+}
 
 function initBoard(width: number, height: number) {
   return Array.from({ length: width * height }, () => ({
@@ -31,22 +38,20 @@ function critical(width: number, height: number, index: number) {
 }
 
 function GamePage() {
-  const intervalRef = useRef<number | null>(null);
   const [iter, setIter] = useState(-1);
-  const [pending, setPending] = useState(false);
   const [board, setBoard] = useState<Cell[]>(initBoard(5, 5));
-  const [isOver, setIsOver] = useState(false);
+  const [status, setStatus] = useState<Status>(Status.Init);
 
   const red = board.filter((cell) => cell.color === Color.Red).length;
   const blue = board.filter((cell) => cell.color === Color.Blue).length;
-
+  const isOver = iter >= 2 && (blue === 0 || red === 0);
   const current = iter === -1 ? Color.None : iter % 2;
 
   function handleClick({ value, color }: Cell, index: number) {
     if (iter === -1) return false;
     // can place on empty cell or same color
     if (color !== Color.None && color !== current) return false;
-    setPending(true);
+    setStatus(Status.Pending);
     setBoard((prev) => {
       const next = [...prev];
       next[index].value = value + 1;
@@ -59,20 +64,20 @@ function GamePage() {
   function handleRestart() {
     setIter(-1);
     setBoard(initBoard(5, 5));
-    setIsOver(false);
-    setPending(false);
+    setStatus(Status.Init);
   }
 
   useEffect(() => {
     if (iter === -1) return;
-    if (iter >= 2 && (blue === 0 || red === 0)) setIsOver(true);
+    if (isOver) setStatus(Status.Over);
     if (board.every((cell, index) => cell.value < critical(5, 5, index))) {
-      const isOver = iter >= 2 && (blue === 0 || red === 0);
-      setPending(false);
-      if (!isOver) setIter((prev) => prev + 1);
+      if (!isOver) {
+        setStatus(Status.Idle);
+        setIter((prev) => prev + 1);
+      }
       return;
     }
-    intervalRef.current = setTimeout(() => {
+    setTimeout(() => {
       setBoard((prev) => {
         const indices = prev
           .map((cell, i) => (cell.value < critical(5, 5, i) ? -1 : i))
@@ -107,12 +112,11 @@ function GamePage() {
   }, [JSON.stringify(board)]);
 
   useEffect(() => {
-    if (isOver) return;
-    setIter(0);
-  }, [isOver]);
+    if (status === Status.Init) setIter(0);
+  }, [status]);
 
   // useEffect(() => {
-  //   if (current !== Color.Red || pending) return;
+  //   if (current !== Color.Red || isPending) return;
   //   fetch("http://localhost:8080/api", {
   //     method: "POST",
   //     headers: {
@@ -125,7 +129,7 @@ function GamePage() {
   //       const index = data.index;
   //       handleClick(board[index], index);
   //     });
-  // }, [current, pending]);
+  // }, [current, isPending]);
 
   return (
     <div
@@ -136,34 +140,30 @@ function GamePage() {
         current === Color.Red && "bg-red-300"
       )}
     >
-      {/* <div className="relative z-10 flex items-center justify-center gap-2 my-4 px-4 py-2 rounded-xl bg-white">
-        <span className="text-2xl text-sky-500">{blue}</span>
-        <span className="text-2xl text-black">·</span>
-        <span className="text-2xl text-red-500">{red}</span>
-      </div> */}
       <div
         className={cn(
           "grid grid-cols-5 grid-rows-5 gap-2",
-          (pending || iter === -1) && "pointer-events-none"
+          (status === "pending" || iter === -1) && "pointer-events-none"
         )}
       >
         {board.map((piece, index) => (
           <Cell
             key={index}
             color={piece.color}
-            isOver={isOver}
+            isOver={status === Status.Over}
             current={current}
             onClick={() => handleClick(piece, index)}
           >
             <Piece value={piece.value} color={piece.color} />
-            {/* <Piece value={6} color={Color.Red} /> */}
           </Cell>
         ))}
       </div>
       <div
         className={cn(
           "absolute inset-0 flex flex-col items-center justify-center gap-4 transition-colors duration-500",
-          isOver ? "bg-black/80 opacity-100" : "pointer-events-none opacity-0"
+          status === Status.Over
+            ? "bg-black/80 opacity-100"
+            : "pointer-events-none opacity-0"
         )}
       >
         {current === Color.Blue && <p className="text-sky-500">Blue Wins!</p>}
