b0c18ba522ed3e3b6fc548341d93298f061986de
guanboo-yang <b09902064@ntu.edu.tw>
2024-08-30 22:25:06 +0800

    feat: use hash history

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createHashHistory,
+  createRouter,
+} from "@tanstack/react-router";
 import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
+const hashHistory = createHashHistory();
+
 // Create a new router instance
 const router = createRouter({
   routeTree,
-  basepath: "/chain-reaction",
+  history: hashHistory,
 });
 
 console.log(router.basepath);
