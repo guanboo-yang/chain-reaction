e4c6914e7c12b65356bfe26f9f515bc31576060d
guanboo-yang <b09902064@ntu.edu.tw>
2024-08-30 21:23:11 +0800

    feat: add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite as tsrv } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/chain-reaction/",
-  plugins: [react()],
+  plugins: [tsrv(), react()],
 });
